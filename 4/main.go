675e6a4a57c4df76e15bbfa4a1ce2db874777279
Michael Lesniak <mail@mlesniak.com>
2019-12-04 07:52:56 +0100

    [4/2] Solution to second part

--- 4/main.go
+++ 4/main.go
@@ -26,7 +26,9 @@ func main() {
 	//fmt.Println(isValidPassword("123789"))
 	//fmt.Println(isValidPassword("223450"))
 	//fmt.Println(isValidPassword("135578"))
-	//fmt.Println(string(135578))
+
+	// Second part:
+	//fmt.Println(isValidPassword("123444"))
 }
 
 func isValidPassword(password string) bool {
@@ -45,8 +47,23 @@ func isValidPassword(password string) bool {
 
 	// Check for at least one pair of double digits.
 	foundDouble := false
-	for idx := 0; idx < len(password)-1; idx++ {
-		if password[idx] == password[idx+1] {
+	// Part 1:
+	//for idx := 0; idx < len(password)-1; idx++ {
+	//	if password[idx] == password[idx+1] {
+	//		foundDouble = true
+	//		break
+	//	}
+	//}
+	// Part 2:
+	// Count number of all digits and check that we have at least one with size 2. This condition should be sufficient
+	// since we also have the increase-condition.
+	counts := make(map[int]int)
+	for idx := 0; idx < len(password); idx++ {
+		number, _ := strconv.Atoi(string(password[idx]))
+		counts[number]++
+	}
+	for _, v := range counts {
+		if v == 2 {
 			foundDouble = true
 			break
 		}

8a73e8fb65bb19fa4b536b2985807d5c26470707
Michael Lesniak <mail@mlesniak.com>
2019-12-05 20:10:43 +0100

    [5/1] Add direction and indirection pointer

--- 5/main.go
+++ 5/main.go
@@ -15,17 +15,55 @@ func main() {
 }
 
 func showResult(memory []int) {
+	//fmt.Println(memory)
 	fmt.Println(memory[0])
 }
 
 func compute(memory []int) {
 	for i := 0; i < len(memory); {
-		switch memory[i] {
+		opcode := memory[i] % 100
+		r1 := memory[i] / 100 % 10
+		r2 := memory[i] / 1000 % 10
+		//r3 := memory[i] / 10000 % 10
+
+		switch opcode {
 		case 1:
-			memory[memory[i+3]] = memory[memory[i+1]] + memory[memory[i+2]]
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[i+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[i+1]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[i+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[i+2]
+			}
+			var m3 int
+			m3 = memory[memory[i+3]]
+			memory[m3] = m1 + m2
 			i += 4
 		case 2:
-			memory[memory[i+3]] = memory[memory[i+1]] * memory[memory[i+2]]
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[i+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[i+1]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[i+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[i+2]
+			}
+			var m3 int
+			m3 = memory[i+3]
+			memory[m3] = m1 * m2
 			i += 4
 		case 3:
 			var num int
@@ -41,6 +79,8 @@ func compute(memory []int) {
 			i += 2
 		case 99:
 			return
+		default:
+			panic("Unknown opcode:" + strconv.Itoa(opcode))
 		}
 	}
 }
@@ -49,7 +89,7 @@ func load() []int {
 	// Import.
 	bytes, _ := ioutil.ReadFile("input.txt")
 	lines := strings.Split(string(bytes), ",")
-	fmt.Println(lines)
+	//fmt.Println(lines)
 	var memory []int
 	for _, val := range lines {
 		i, _ := strconv.Atoi(val)

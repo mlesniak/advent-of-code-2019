d38272697ef751f8e752470d7e6961ad803a0fdb
Michael Lesniak <mail@mlesniak.com>
2019-12-05 19:50:15 +0100

    [5/1] Add input and output

--- 5/main.go
+++ 5/main.go
@@ -19,14 +19,28 @@ func showResult(memory []int) {
 }
 
 func compute(memory []int) {
-	for i := 0; i < len(memory); i += 4 {
+	for i := 0; i < len(memory); {
 		switch memory[i] {
 		case 1:
 			memory[memory[i+3]] = memory[memory[i+1]] + memory[memory[i+2]]
+			i += 4
 		case 2:
 			memory[memory[i+3]] = memory[memory[i+1]] * memory[memory[i+2]]
+			i += 4
+		case 3:
+			var num int
+			fmt.Print("? ")
+			_, err := fmt.Scanf("%d", &num)
+			if err != nil {
+				panic(err)
+			}
+			memory[memory[i+1]] = num
+			i += 2
+		case 4:
+			fmt.Println(memory[memory[i+1]])
+			i += 2
 		case 99:
-			break
+			return
 		}
 	}
 }
@@ -35,6 +49,7 @@ func load() []int {
 	// Import.
 	bytes, _ := ioutil.ReadFile("input.txt")
 	lines := strings.Split(string(bytes), ",")
+	fmt.Println(lines)
 	var memory []int
 	for _, val := range lines {
 		i, _ := strconv.Atoi(val)

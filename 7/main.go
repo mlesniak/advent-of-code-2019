8d6b2039e6844794f4b14e291f3371a5b511fb5f
Michael Lesniak <mail@mlesniak.com>
2019-12-07 07:12:32 +0100

    [7/1] Initial commit for day 7

---
+++ 7/main.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// Part 1.
+	memory := load()
+	compute(memory)
+	//showResult(memory)
+}
+
+func showResult(memory []int) {
+	//fmt.Println(memory)
+	fmt.Println(memory[0])
+}
+
+func compute(memory []int) {
+	for ip := 0; ip < len(memory); {
+		//fmt.Println("-------------------------------------------------")
+		//fmt.Println("*** ip:", ip)
+		//for i, k := range memory {
+		//	fmt.Println(i, "op:",k)
+		//}
+		mem := memory[ip]
+		opcode := mem % 100
+		r1 := mem / 100 % 10
+		r2 := mem / 1000 % 10
+		//r3 := memory[ip] / 10000 % 10
+
+		switch opcode {
+		case 1:
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			var m3 int
+			m3 = memory[ip+3]
+			memory[m3] = m1 + m2
+			ip += 4
+		case 2:
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			var m3 int
+			m3 = memory[ip+3]
+			memory[m3] = m1 * m2
+			ip += 4
+		case 3:
+			var num int
+			fmt.Print("? ")
+			_, err := fmt.Scanf("%d", &num)
+			if err != nil {
+				panic(err)
+			}
+			memory[memory[ip+1]] = num
+			ip += 2
+		case 4:
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			fmt.Println(m1)
+			ip += 2
+		case 5:
+			// Jump if true.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if m1 != 0 {
+				ip = m2
+			} else {
+				ip += 3
+			}
+		case 6:
+			// Jump if false.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if m1 == 0 {
+				ip = m2
+			} else {
+				ip += 3
+			}
+		case 7:
+			// Less than.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			var m3 int
+			m3 = memory[ip+3]
+			if m1 < m2 {
+				memory[m3] = 1
+			} else {
+				memory[m3] = 0
+			}
+			ip += 4
+		case 8:
+			// Equals.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			var m3 int
+			m3 = memory[ip+3]
+			if m1 == m2 {
+				memory[m3] = 1
+			} else {
+				memory[m3] = 0
+			}
+			ip += 4
+		case 99:
+			return
+		default:
+			panic("Unknown opcode:" + strconv.Itoa(opcode))
+		}
+	}
+}
+
+func load() []int {
+	// Import.
+	bytes, _ := ioutil.ReadFile("input.txt")
+	lines := strings.Split(string(bytes), ",")
+	//fmt.Println(lines)
+	var memory []int
+	for _, val := range lines {
+		i, _ := strconv.Atoi(val)
+		memory = append(memory, i)
+	}
+	return memory
+}

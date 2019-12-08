cff58156e931d1afb5b1e7ed7ed473c784f63de6
Michael Lesniak <mail@mlesniak.com>
2019-12-08 10:56:34 +0100

    [8/1] Create data structure

--- 8/main.go
+++ 8/main.go
@@ -15,9 +15,28 @@ type image struct {
 }
 
 func main() {
+	width := 3
+	height := 2
 	data := load()
-	fmt.Println(data[:10])
 
+	image := image{width: width, height: height}
+
+	// Read image parts
+	layerSize := width * height
+	for layerIndex := 0; layerIndex < len(data)/layerSize; layerIndex++ {
+		layerData := data[layerIndex*layerSize : layerIndex*layerSize+layerSize]
+		layer := make([][]int, height)
+		for row := 0; row < height; row++ {
+			layer[row] = make([]int, width)
+			for col := 0; col < width; col++ {
+				idx := row*width + col
+				layer[row][col] = layerData[idx]
+			}
+		}
+		image.layers = append(image.layers, layer)
+	}
+
+	fmt.Println(image)
 }
 
 func load() []int {

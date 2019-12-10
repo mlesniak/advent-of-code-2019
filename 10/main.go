13e94ecf33b6e61617ab1b2315f60bdb19a966ed
Michael Lesniak <mail@mlesniak.com>
2019-12-10 11:07:28 +0100

    [10/1] Compute line of sights

--- 10/main.go
+++ 10/main.go
@@ -7,8 +7,48 @@ import (
 )
 
 func main() {
-	memory := load()
-	fmt.Println(memory)
+	asteroids := load()
+	fmt.Println(asteroids)
+
+	// Determine maximum dimensions.
+	maxX := 0
+	maxY := 0
+	for c := range asteroids {
+		if c.x > maxX {
+			maxX = c.x
+		}
+		if c.y > maxY {
+			maxY = c.y
+		}
+	}
+
+	// Compute hidden line of sight by computing x and y deltas.
+	for asteroid := range asteroids {
+		// Create a copy of the whole map.
+		for candidate := range asteroids {
+			if asteroid == candidate {
+				continue
+			}
+			dx := candidate.x - asteroid.x
+			dy := candidate.y - asteroid.y
+			fmt.Println(asteroid, candidate, dx, dy)
+
+			// Remove all elements on the path from asteroid + delta
+			px := asteroid.x
+			py := asteroid.y
+			for {
+				if !(px >= 0 && px <= maxX && py >= 0 && py <= maxY) {
+					break
+				}
+				fmt.Println("  px=", px, "py=", py)
+				px += dx
+				py += dy
+			}
+
+			// Count remaining asteroids in copy.
+			// Remember largest map.
+		}
+	}
 }
 
 type coordinate struct {

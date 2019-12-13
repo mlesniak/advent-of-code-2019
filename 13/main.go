284253973b74296cc28eb3c414067b8db8cba42f
Michael Lesniak <mail@mlesniak.com>
2019-12-13 08:52:46 +0100

    [13/1] Working first part

---
+++ 13/main.go
@@ -0,0 +1,356 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const MemorySize = 1000000
+
+func main() {
+	// Create canvas.
+	canvas := make([][]int, 24)
+	for row := range canvas {
+		canvas[row] = make([]int, 50)
+	}
+
+	memory := load()
+	in := newChannel()
+	out := newChannel()
+	go func() {
+		for {
+			x := <-out
+			y := <-out
+			t := <-out
+			canvas[y][x] = t
+		}
+	}()
+
+	// Necessary?
+	in <- 1
+
+	compute("memory", memory, in, out)
+	time.Sleep(time.Second)
+
+	// Paint whole canvas.
+	paintCanvas(canvas)
+
+	// Count blocks.
+	blocks := 0
+	forCanvas(canvas, func(x, y, val int) {
+		if val == 2 {
+			blocks++
+		}
+	})
+	println(blocks)
+}
+
+func forCanvas(canvas [][]int, f func(int, int, int)) {
+	for row := range canvas {
+		for col := range canvas[row] {
+			f(col, row, canvas[row][col])
+		}
+	}
+}
+
+func paintCanvas(canvas [][]int) {
+	cmd := exec.Command("clear")
+	//Linux example, its tested
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+	for row := range canvas {
+		for col := range canvas[row] {
+			var c string
+			switch canvas[row][col] {
+			case 0:
+				c = " "
+			case 1:
+				c = "█"
+			case 2:
+				c = "⏹"
+			case 3:
+				c = "🕳"
+			case 4:
+				c = "⚽️"
+			}
+			fmt.Print(c)
+		}
+		fmt.Println()
+	}
+}
+
+func newChannel() chan int {
+	channelSize := 10
+	return make(chan int, channelSize)
+}
+
+func compute(name string, memory []int, in chan int, out chan int) {
+	relBase := 0
+
+	for ip := 0; ip < len(memory); {
+		mem := memory[ip]
+		opcode := mem % 100
+		r1 := mem / 100 % 10
+		r2 := mem / 1000 % 10
+		r3 := memory[ip] / 10000 % 10
+
+		switch opcode {
+		case 1:
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			var m3 int
+			if r3 == 0 {
+				m3 = memory[ip+3]
+			}
+			if r3 == 2 {
+				m3 = memory[ip+3] + relBase
+			}
+			memory[m3] = m1 + m2
+			ip += 4
+		case 2:
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			var m3 int
+			if r3 == 0 {
+				m3 = memory[ip+3]
+			}
+			if r3 == 1 {
+				panic("TODO")
+			}
+			if r3 == 2 {
+				m3 = memory[ip+3] + relBase
+			}
+			memory[m3] = m1 * m2
+			ip += 4
+		case 3:
+			var num int
+			num = <-in
+			if r1 == 0 {
+				memory[memory[ip+1]] = num
+			}
+			if r1 == 1 {
+				panic("How should this work?")
+			}
+			if r1 == 2 {
+				memory[memory[ip+1]+relBase] = num
+			}
+			ip += 2
+		case 4:
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			out <- m1
+			ip += 2
+		case 5:
+			// Jump if true.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			if m1 != 0 {
+				ip = m2
+			} else {
+				ip += 3
+			}
+		case 6:
+			// Jump if false.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			if m1 == 0 {
+				ip = m2
+			} else {
+				ip += 3
+			}
+		case 7:
+			// Less than.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			var m3 int
+			if r3 == 0 {
+				m3 = memory[ip+3]
+			}
+			if r3 == 1 {
+				panic("TODO")
+			}
+			if r3 == 2 {
+				m3 = memory[ip+3] + relBase
+			}
+			if m1 < m2 {
+				memory[m3] = 1
+			} else {
+				memory[m3] = 0
+			}
+			ip += 4
+		case 8:
+			// Equals.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			var m3 int
+			if r3 == 0 {
+				m3 = memory[ip+3]
+			}
+			if r3 == 1 {
+				panic("TODO")
+			}
+			if r3 == 2 {
+				m3 = memory[ip+3] + relBase
+			}
+			if m1 == m2 {
+				memory[m3] = 1
+			} else {
+				memory[m3] = 0
+			}
+			ip += 4
+		case 9:
+			// Relative base adjustment.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			relBase += m1
+			ip += 2
+		case 99:
+			return
+		default:
+			panic("Unknown opcode:" + strconv.Itoa(opcode))
+		}
+	}
+}
+
+func load() []int {
+	bytes, _ := ioutil.ReadFile("input.txt")
+	lines := strings.Split(string(bytes), ",")
+	memory := make([]int, MemorySize)
+	for idx, val := range lines {
+		i, err := strconv.Atoi(val)
+		if err != nil {
+			panic(err)
+		}
+		memory[idx] = i
+	}
+	return memory
+}

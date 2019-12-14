0a122953e87d07e3bfe2cd107a03270c376f8c8b
Michael Lesniak <mail@mlesniak.com>
2019-12-14 07:46:03 +0100

    [14/1] Add demo data

--- 14/main.go
+++ 14/main.go
@@ -7,13 +7,6 @@ import (
 	"strings"
 )
 
-//10 ORE => 10 A
-//1 ORE => 1 B
-//7 A, 1 B => 1 C
-//7 A, 1 C => 1 D
-//7 A, 1 D => 1 E
-//7 A, 1 E => 1 FUEL
-
 type chemical struct {
 	name     string
 	quantity int

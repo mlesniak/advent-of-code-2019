c68d5c4320a7e5f640bfd99edd153c5890a47d09
Michael Lesniak <mail@mlesniak.com>
2019-12-17 08:44:21 +0100

    [17/1] Initial setup and visualization

---
+++ 17/main.go
@@ -0,0 +1,329 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func debug(a ...interface{}) {
+	fmt.Println(a...)
+}
+
+func main() {
+	memory, in, out, stop := load()
+
+	go func() {
+		for {
+			if *stop {
+				break
+			}
+			c := <-out
+			fmt.Print(string(c))
+		}
+	}()
+	compute(memory, in, out, stop)
+}
+
+func compute(memory []int, in chan int, out chan int, stop *bool) {
+	relBase := 0
+
+	for ip := 0; ip < len(memory); {
+		mem := memory[ip]
+		opcode := mem % 100
+		r1 := mem / 100 % 10
+		r2 := mem / 1000 % 10
+		r3 := memory[ip] / 10000 % 10
+
+		switch opcode {
+		case 1:
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			var m3 int
+			if r3 == 0 {
+				m3 = memory[ip+3]
+			}
+			if r3 == 2 {
+				m3 = memory[ip+3] + relBase
+			}
+			memory[m3] = m1 + m2
+			ip += 4
+		case 2:
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			var m3 int
+			if r3 == 0 {
+				m3 = memory[ip+3]
+			}
+			if r3 == 1 {
+				panic("TODO")
+			}
+			if r3 == 2 {
+				m3 = memory[ip+3] + relBase
+			}
+			memory[m3] = m1 * m2
+			ip += 4
+		case 3:
+			var num int
+			num = <-in
+			if r1 == 0 {
+				memory[memory[ip+1]] = num
+			}
+			if r1 == 1 {
+				panic("How should this work?")
+			}
+			if r1 == 2 {
+				memory[memory[ip+1]+relBase] = num
+			}
+			ip += 2
+		case 4:
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			out <- m1
+			ip += 2
+		case 5:
+			// Jump if true.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			if m1 != 0 {
+				ip = m2
+			} else {
+				ip += 3
+			}
+		case 6:
+			// Jump if false.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			if m1 == 0 {
+				ip = m2
+			} else {
+				ip += 3
+			}
+		case 7:
+			// Less than.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			var m3 int
+			if r3 == 0 {
+				m3 = memory[ip+3]
+			}
+			if r3 == 1 {
+				panic("TODO")
+			}
+			if r3 == 2 {
+				m3 = memory[ip+3] + relBase
+			}
+			if m1 < m2 {
+				memory[m3] = 1
+			} else {
+				memory[m3] = 0
+			}
+			ip += 4
+		case 8:
+			// Equals.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			var m2 int
+			if r2 == 0 {
+				m2 = memory[memory[ip+2]]
+			}
+			if r2 == 1 {
+				m2 = memory[ip+2]
+			}
+			if r2 == 2 {
+				m2 = memory[memory[ip+2]+relBase]
+			}
+			var m3 int
+			if r3 == 0 {
+				m3 = memory[ip+3]
+			}
+			if r3 == 1 {
+				panic("TODO")
+			}
+			if r3 == 2 {
+				m3 = memory[ip+3] + relBase
+			}
+			if m1 == m2 {
+				memory[m3] = 1
+			} else {
+				memory[m3] = 0
+			}
+			ip += 4
+		case 9:
+			// Relative base adjustment.
+			var m1 int
+			if r1 == 0 {
+				m1 = memory[memory[ip+1]]
+			}
+			if r1 == 1 {
+				m1 = memory[ip+1]
+			}
+			if r1 == 2 {
+				m1 = memory[memory[ip+1]+relBase]
+			}
+			relBase += m1
+			ip += 2
+		case 99:
+			*stop = true
+			return
+		default:
+			panic("Unknown opcode:" + strconv.Itoa(opcode))
+		}
+	}
+}
+
+const MemorySize = 1000000
+const ChannelSize = 1
+
+func load() ([]int, chan int, chan int, *bool) {
+	bytes, _ := ioutil.ReadFile("input.txt")
+	lines := strings.Split(string(bytes), ",")
+	memory := make([]int, MemorySize)
+	for idx, val := range lines {
+		i, err := strconv.Atoi(val)
+		if err != nil {
+			panic(err)
+		}
+		memory[idx] = i
+	}
+	in := make(chan int, ChannelSize)
+	out := make(chan int, ChannelSize)
+	stop := false
+	return memory, in, out, &stop
+}
+
+func drawShip(ship [][]int) {
+	x := len(ship[0]) / 2
+	y := len(ship) / 2
+
+	//os.Stdout.WriteString("\x1b[3;J\x1b[H\x1b[2J")
+	for row := range ship {
+		for col := range ship[row] {
+			var c string
+			switch ship[row][col] {
+			case -1:
+				c = "_"
+			case 0:
+				c = "#"
+			case 1:
+				c = "."
+			case 2:
+				c = "O️"
+			case 3:
+				c = "S"
+			}
+			if col == x && row == y {
+				c = "X"
+			}
+			fmt.Print(c)
+		}
+		fmt.Println()
+	}
+}

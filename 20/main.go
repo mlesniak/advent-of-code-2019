561774fe029df9a2090c5bf1594b64197888ddbd
Michael Lesniak <mail@mlesniak.com>
2019-12-24 13:01:12 +0100

    [20/1] Improve data structure

--- 20/main.go
+++ 20/main.go
@@ -17,6 +17,7 @@ type point struct {
 type maze struct {
 	data   [][]int
 	portal map[point]point
+	gates  map[string][]point
 }
 
 func main() {
@@ -37,32 +38,53 @@ func load() maze {
 		}
 	}
 
-	// Find portals.
-	portals := make(map[point]point)
+	type pointDir struct {
+		point
+		//    1
+		//   3 4
+		//    2
+		orientation int
+	}
+
+	// Find gates.
+	gates := make(map[string][]pointDir)
 	for y := 0; y < len(data); y++ {
 		for x := 0; x < len(data[y]); x++ {
 			if data[y][x] >= 'A' && data[y][x] <= 'Z' {
 				// Look around if a corresponding dot can be found.
+				var id string
+				var p pointDir
 				if y > 0 && data[y-1][x] == '.' {
-					id := SortString(string(data[y+1][x]) + string(data[y][x]))
-					fmt.Println(id, x, y, "up")
+					id = SortString(string(data[y+1][x]) + string(data[y][x]))
+					p = pointDir{point{x, y}, 1}
 				}
 				if y < len(data)-1 && data[y+1][x] == '.' {
-					id := SortString(string(data[y-1][x]) + string(data[y][x]))
-					fmt.Println(id, x, y, "down")
+					id = SortString(string(data[y-1][x]) + string(data[y][x]))
+					p = pointDir{point{x, y}, 2}
 				}
 				if x < len(data[0])-1 && data[y][x+1] == '.' {
-					id := SortString(string(data[y][x-1]) + string(data[y][x]))
-					fmt.Println(id, x, y, "right")
+					id = SortString(string(data[y][x-1]) + string(data[y][x]))
+					p = pointDir{point{x, y}, 4}
 				}
 				if x > 0 && data[y][x-1] == '.' {
-					id := SortString(string(data[y][x+1]) + string(data[y][x]))
-					fmt.Println(id, x, y, "left")
+					id = SortString(string(data[y][x+1]) + string(data[y][x]))
+					p = pointDir{point{x, y}, 3}
+				}
+				if id != "" {
+					gates[id] = append(gates[id], p)
 				}
 			}
 		}
 	}
 
+	for key, value := range gates {
+		// Ignore start and end gate.
+		if len(value) > 1 {
+
+		}
+		fmt.Println(key, value)
+	}
+	portals := make(map[point]point)
 	return maze{data: data, portal: portals}
 }
 

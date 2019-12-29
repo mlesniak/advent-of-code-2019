925a2c076fac07a6de6fabfc676854f424119d76
Michael Lesniak <mail@mlesniak.com>
2019-12-29 07:32:51 +0100

    [22/1] Initial setup

---
+++ 22/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type deck struct {
+	cards []int
+}
+
+func (d deck) String() string {
+	return fmt.Sprintf("CARDS: %v", d.cards)
+}
+
+func newDeck(size int) deck {
+	nd := make([]int, size)
+	for i := 0; i < size; i++ {
+		nd[i] = i
+	}
+
+	return deck{cards: nd}
+}
+
+func (d deck) deal() deck {
+	nd := make([]int, len(d.cards))
+	for index, value := range d.cards {
+		nd[len(nd)-1-index] = value
+	}
+
+	return deck{cards: nd}
+}
+
+func main() {
+	d := newDeck(10)
+	d2 := d.deal()
+	fmt.Println(d2)
+}
+
+// TODO Parse into commands
+func load() []string {
+	bytes, _ := ioutil.ReadFile("input.txt")
+	lines := strings.Split(string(bytes), "\n")
+
+	return lines
+}
+
+func wait() {
+	fmt.Print("<ENTER>")
+	bufio.NewReader(os.Stdin).ReadLine()
+}

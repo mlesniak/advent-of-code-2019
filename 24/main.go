713cf1f29aaf0f8890453b0362e70274177c5f5c
Michael Lesniak <mail@mlesniak.com>
2020-01-08 17:17:08 +0100

    [24/2] Update Next() function using maps

--- 24/main.go
+++ 24/main.go
@@ -33,39 +33,35 @@ func (a area) Score() int {
 	return sum
 }
 
-//func (a area) Next() area {
-//	// Init new area.
-//	b := make([][]int, len(a))
-//	for i, _ := range a[0] {
-//		b[i] = make([]int, len(a[0]))
-//	}
-//
-//	for row, line := range a {
-//		for col, value := range line {
-//			ns := a.Neighbors(row, col)
-//
-//			// Is there a bug?
-//			if value == 1 {
-//				if ns != 1 {
-//					b[row][col] = 0
-//				} else {
-//					b[row][col] = 1
-//				}
-//			}
-//
-//			// Empty space.
-//			if value == 0 {
-//				if ns == 1 || ns == 2 {
-//					b[row][col] = 1
-//				} else {
-//					b[row][col] = 0
-//				}
-//			}
-//		}
-//	}
-//
-//	return b
-//}
+func (a area) Next() area {
+	b := make(map[point]bool)
+
+	for row := 0; row < 5; row++ {
+		for col := 0; col < 5; col++ {
+			ns := a.Neighbors(row, col)
+
+			// Bug
+			if a[point{row, col}] {
+				if ns == 1 {
+					b[point{row, col}] = true
+				} else {
+					b[point{row, col}] = false
+				}
+			}
+
+			// Empty space.
+			if !a[point{row, col}] {
+				if ns == 1 || ns == 2 {
+					b[point{row, col}] = true
+				} else {
+					b[point{row, col}] = false
+				}
+			}
+		}
+	}
+
+	return b
+}
 
 func (a area) String() string {
 	s := ""
@@ -85,46 +81,42 @@ func (a area) String() string {
 	return s
 }
 
-//func (a area) Neighbors(row int, col int) int {
-//	ns := 0
-//
-//	// Up
-//	if row > 0 && a[row-1][col] == 1 {
-//		ns++
-//	}
-//	// Down
-//	if row < len(a)-1 && a[row+1][col] == 1 {
-//		ns++
-//	}
-//	// Left
-//	if col > 0 && a[row][col-1] == 1 {
-//		ns++
-//	}
-//	// Right
-//	if col < len(a[row])-1 && a[row][col+1] == 1 {
-//		ns++
-//	}
-//
-//	return ns
-//}
+func (a area) Neighbors(row int, col int) int {
+	ns := 0
+
+	if a[point{row - 1, col}] {
+		ns++
+	}
+	if a[point{row + 1, col}] {
+		ns++
+	}
+	if a[point{row, col - 1}] {
+		ns++
+	}
+	if a[point{row, col + 1}] {
+		ns++
+	}
+
+	return ns
+}
 
 func main() {
 	a := load()
 	fmt.Println(a)
-	fmt.Println(a.Score())
+	//fmt.Println(a.Score())
 
-	//history := make(map[int]bool)
-	//for {
-	//	a = a.Next()
-	//	fmt.Println(a)
-	//	score := a.Score()
-	//	fmt.Println(score)
-	//	if history[score] {
-	//		fmt.Println(score)
-	//		break
-	//	}
-	//	history[score] = true
-	//}
+	history := make(map[int]bool)
+	for {
+		a = a.Next()
+		fmt.Println(a)
+		score := a.Score()
+		fmt.Println(score)
+		if history[score] {
+			fmt.Println(score)
+			break
+		}
+		history[score] = true
+	}
 }
 
 func load() area {

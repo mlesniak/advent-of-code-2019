9a2e7cb377f19b3602022ecc08fb953c06e58fd4
Michael Lesniak <mail@mlesniak.com>
2020-01-08 16:29:56 +0100

    [24/1] Working next generation computation function

--- 24/main.go
+++ 24/main.go
@@ -27,6 +27,40 @@ func (a area) Score() int {
 	return sum
 }
 
+func (a area) Next() area {
+	// Init new area.
+	b := make([][]int, len(a))
+	for i, _ := range a[0] {
+		b[i] = make([]int, len(a[0]))
+	}
+
+	for row, line := range a {
+		for col, value := range line {
+			ns := a.Neighbors(row, col)
+
+			// Is there a bug?
+			if value == 1 {
+				if ns != 1 {
+					b[row][col] = 0
+				} else {
+					b[row][col] = 1
+				}
+			}
+
+			// Empty space.
+			if value == 0 {
+				if ns == 1 || ns == 2 {
+					b[row][col] = 1
+				} else {
+					b[row][col] = 0
+				}
+			}
+		}
+	}
+
+	return b
+}
+
 func (a area) String() string {
 	s := ""
 
@@ -45,10 +79,38 @@ func (a area) String() string {
 	return s
 }
 
+func (a area) Neighbors(row int, col int) int {
+	ns := 0
+
+	// Up
+	if row > 0 && a[row-1][col] == 1 {
+		ns++
+	}
+	// Down
+	if row < len(a)-1 && a[row+1][col] == 1 {
+		ns++
+	}
+	// Left
+	if col > 0 && a[row][col-1] == 1 {
+		ns++
+	}
+	// Right
+	if col < len(a[row])-1 && a[row][col+1] == 1 {
+		ns++
+	}
+
+	return ns
+}
+
 func main() {
 	a := load()
 	fmt.Println(a)
-	fmt.Println(a.Score())
+	for {
+		a = a.Next()
+		fmt.Println(a)
+		fmt.Println(a.Score())
+		wait()
+	}
 }
 
 func load() area {
